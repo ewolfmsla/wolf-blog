97cbc07b14eab9e6391bdf82b296f7a41dc2125d
Eric Wolf <ewolf49@gmail.com>
2022-01-04 15:59:28 -0700

    wolf/log-changes changed the styling on log component

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -2,7 +2,12 @@ import React from "react"
 import { FaArrowRight } from "react-icons/fa"
 
 const Logo = () => {
-  return <span className={'logo'}>No Outlet <FaArrowRight color={'#ffff00'} /> Blog</span>
+  return <span className={'logoBorderOuter'}>
+    <span className={'logoBorder'}>
+      <span className={'logo'}>No Outlet <FaArrowRight color={'#000000'} size={25} />
+      </span>
+    </span>
+  </span>
 }
 
 export default Logo
